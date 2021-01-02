210673a2ee3ad4379cfc277ee4aa04b36dea6752
Leonardo <lbellaera@fi.uba.ar>
2021-01-02 20:47:26 -0300

    Modifies urlNode with infura url and contract address

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  urlNode: 'http://localhost:8545',
-  contractAddress: '0x0000000000000000000000000000000000000000',
+  urlNode: 'https://ropsten.infura.io/v3/2d36a52ff76f4b538198d9b2568b4f37',
+  contractAddress: '0x9499f50e3eea5735597C4b89A4D3806a74d970E3',
 };
 
 module.exports = config;
