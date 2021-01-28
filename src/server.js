92a91f3c16758a9954b173e80efb6754e64adf2d
Leonardo <lbellaera@fi.uba.ar>
2021-01-28 03:06:47 -0300

    Heroku deploy fix

--- src/server.js
+++ src/server.js
@@ -7,12 +7,11 @@ const fastify = require('fastify')({ logger: true });
 
 // Declares routes
 routes.forEach((route) => fastify.route(route({ config, services })));
-const port = process.env.PORT || 3000;
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(port);
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
